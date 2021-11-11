2ee56f5b7e6978e146c409e5bcd49b96fb808745
Patrick Abel-Nwachukwu <Patrick>
2021-11-11 16:40:56 -0700

    initial commit

---
+++ src/Deck.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Animated, Text } from "react-native";
+
+class Deck extends Component {
+  renderCard(item) {
+    return <Text>{item.text}</Text>;
+  }
+
+  renderCards() {
+    return this.props.data.map((item) => {
+      return this.renderCard(item);
+    });
+  }
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
+}
+
+export default Deck;
